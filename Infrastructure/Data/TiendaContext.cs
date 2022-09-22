61b35fe44d96282c9b24866477702238c61dd6bf
TorresMBA <bryan98tm@gmail.com>
2022-09-22 17:22:07 -0500

    Initial Commit - Arquitectura del Proyecto y Migración Inicial

---
+++ Infrastructure/Data/TiendaContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class TiendaContext : DbContext
+    {
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+        public TiendaContext(DbContextOptions options) : base(options)
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
